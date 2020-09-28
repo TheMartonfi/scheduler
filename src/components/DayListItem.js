589cb397b3ddacfc341bfffd7fee85199b088599
Carl Comtois <carlmartonfi@hotmail.com>
2020-09-28 16:52:52 -0600

    Made Button component and started working on DayListItem component

---
+++ src/components/DayListItem.js
@@ -0,0 +1,17 @@
+import React from "react";
+import classnames from "classnames";
+import "components/DayListItem.scss";
+
+export default function DayListItem(props) {
+  const dayListItemClass = classnames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots
+  });
+
+  return (
+    <li className={dayListItemClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2> 
+      <h3 className="text--light">{props.spots}</h3>
+    </li>
+  );
+};
