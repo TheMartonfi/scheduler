53347a8165d706df1851f8b786761eadda7eaeb3
Carl Comtois <carlmartonfi@hotmail.com>
2020-09-29 16:03:30 -0600

    Make Show component with Story

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import "components/Appointment/styles.scss";
 import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 
 const Appointment = (props) => {
 
