f8f4558de85552d237eda0601881615eb8c5b4bb
Carl Comtois <carlmartonfi@hotmail.com>
2020-10-01 17:18:02 -0600

    Make selector for getting interviewers by day with tests

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,8 +3,7 @@ import axios from "axios";
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
-import { getAppointmentsForDay } from "helpers/selectors";
-import { getInterview } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 export default function Application(props) {
 
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -18,3 +18,19 @@ export function getInterview(state, interview) {
   return interview ? { ...interview, interviewer: state.interviewers[String(interview.interviewer)] } : null;
 };
 
+export function getInterviewersForDay(state, day) {
+  
+  const interviewers = []; 
+
+  state.days.forEach((dayObject) => {
+    if (dayObject.name === day) {
+      dayObject.interviewers.forEach((interviewer) => {
+        interviewers.push(state.interviewers[interviewer]);
+      });
+    }
+  });
+
+  return interviewers;
+
+};
+
