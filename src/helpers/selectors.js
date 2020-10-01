9d9a933f4b88558316555c4cb3d253c311e25944
Carl Comtois <carlmartonfi@hotmail.com>
2020-09-30 18:53:53 -0600

    Refactor get appointments by day selector

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,18 +1,14 @@
 export function getAppointmentsForDay(state, day) {
 
   const appointments = []; 
-  let appointmentsId;
 
   state.days.forEach((dayObject) => {
     if (dayObject.name === day) {
-      appointmentsId = dayObject.appointments
+      dayObject.appointments.forEach((appointment) => {
+        appointments.push(state.appointments[appointment]);
+      });
     }
   });
-
-  appointmentsId && appointmentsId.forEach((appointmentId) => {
-    appointments.push(state.appointments[appointmentId]);
-  });
-
 
   return appointments;
 };
