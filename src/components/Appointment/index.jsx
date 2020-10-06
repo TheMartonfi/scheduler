4fbc9e6852d60e1318821634358aa0b0249bd8bb
Carl Comtois <carlmartonfi@hotmail.com>
2020-10-06 14:03:42 -0600

    Make test to emulate user booking interview and added put method to axios mock

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -20,96 +20,96 @@ const ERROR_SAVE = "ERROR_SAVE";
 const ERROR_DELETE = "ERROR_DELETE";
 
 const Appointment = props => {
-  const { mode, transition, back } = useVisualMode(
-    props.interview ? SHOW : EMPTY
-  );
+	const { mode, transition, back } = useVisualMode(
+		props.interview ? SHOW : EMPTY
+	);
 
-  React.useEffect(() => {
-    if (props.interview && mode === EMPTY) {
-      transition(SHOW);
-    }
-    if (props.interview === null && mode === SHOW) {
-      transition(EMPTY);
-    }
-  }, [props.interview, transition, mode]);
+	React.useEffect(() => {
+		if (props.interview && mode === EMPTY) {
+			transition(SHOW);
+		}
+		if (props.interview === null && mode === SHOW) {
+			transition(EMPTY);
+		}
+	}, [props.interview, transition, mode]);
 
-  const save = (name, interviewer) => {
-    const interview = {
-      student: name,
-      interviewer,
-    };
+	const save = (name, interviewer) => {
+		const interview = {
+			student: name,
+			interviewer,
+		};
 
-    transition(SAVING);
-    props
-      .bookInterview(props.id, interview)
-      .then(() => {
-        transition(SHOW);
-      })
-      .catch(() => {
-        transition(ERROR_SAVE, true);
-      });
-  };
+		transition(SAVING);
+		props
+			.bookInterview(props.id, interview)
+			.then(() => {
+				transition(SHOW);
+			})
+			.catch(() => {
+				transition(ERROR_SAVE, true);
+			});
+	};
 
-  const destroy = () => {
-    transition(DELETING, true);
-    props
-      .cancelInterview(props.id)
-      .then(() => {
-        transition(EMPTY);
-      })
-      .catch(() => {
-        transition(ERROR_DELETE, true);
-      });
-  };
+	const destroy = () => {
+		transition(DELETING, true);
+		props
+			.cancelInterview(props.id)
+			.then(() => {
+				transition(EMPTY);
+			})
+			.catch(() => {
+				transition(ERROR_DELETE, true);
+			});
+	};
 
-  return (
-    <article className="appointment">
-      <Header time={props.time} />
-      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
-      {mode === SHOW && props.interview && (
-        <Show
-          student={props.interview.student}
-          interviewer={props.interview.interviewer.name}
-          onEdit={() => transition(EDIT)}
-          onDelete={() => transition(CONFIRM)}
-        />
-      )}
-      {mode === CREATE && (
-        <Form
-          interviewers={props.interviewers}
-          onCancel={() => back()}
-          onSave={save}
-        />
-      )}
-      {mode === SAVING && <Status message={SAVING} />}
-      {mode === DELETING && <Status message={DELETING} />}
-      {mode === CONFIRM && (
-        <Confirm
-          onCancel={() => back()}
-          onConfirm={() => destroy()}
-          message={"Are you sure you would like to delete?"}
-        />
-      )}
-      {mode === EDIT && (
-        <Form
-          interviewers={props.interviewers}
-          name={props.interview.student}
-          interviewer={props.interview.interviewer.id}
-          onCancel={() => back()}
-          onSave={save}
-        />
-      )}
-      {mode === ERROR_SAVE && (
-        <Error onClose={() => back()} message={"Could not book appointment."} />
-      )}
-      {mode === ERROR_DELETE && (
-        <Error
-          onClose={() => back()}
-          message={"Could not cancel appointment."}
-        />
-      )}
-    </article>
-  );
+	return (
+		<article className="appointment" data-testid="appointment">
+			<Header time={props.time} />
+			{mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+			{mode === SHOW && props.interview && (
+				<Show
+					student={props.interview.student}
+					interviewer={props.interview.interviewer.name}
+					onEdit={() => transition(EDIT)}
+					onDelete={() => transition(CONFIRM)}
+				/>
+			)}
+			{mode === CREATE && (
+				<Form
+					interviewers={props.interviewers}
+					onCancel={() => back()}
+					onSave={save}
+				/>
+			)}
+			{mode === SAVING && <Status message={SAVING} />}
+			{mode === DELETING && <Status message={DELETING} />}
+			{mode === CONFIRM && (
+				<Confirm
+					onCancel={() => back()}
+					onConfirm={() => destroy()}
+					message={"Are you sure you would like to delete?"}
+				/>
+			)}
+			{mode === EDIT && (
+				<Form
+					interviewers={props.interviewers}
+					name={props.interview.student}
+					interviewer={props.interview.interviewer.id}
+					onCancel={() => back()}
+					onSave={save}
+				/>
+			)}
+			{mode === ERROR_SAVE && (
+				<Error onClose={() => back()} message={"Could not book appointment."} />
+			)}
+			{mode === ERROR_DELETE && (
+				<Error
+					onClose={() => back()}
+					message={"Could not cancel appointment."}
+				/>
+			)}
+		</article>
+	);
 };
 
 export default Appointment;
--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,19 +1,48 @@
 import React from "react";
 import {
-  render,
-  cleanup,
-  waitForElement,
-  fireEvent,
+	render,
+	cleanup,
+	waitForElement,
+	fireEvent,
+	getByText,
+	prettyDOM,
+	getByTestId,
+	getAllByTestId,
+	getByAltText,
 } from "@testing-library/react";
 import Application from "components/Application";
 
 afterEach(cleanup);
 
-it("defaults to Monday and changes the schedule when a new day is selected", () => {
-  const { getByText } = render(<Application />);
+describe("Application", () => {
+	it("defaults to Monday and changes the schedule when a new day is selected", async () => {
+		const { getByText } = render(<Application />);
 
-  return waitForElement(() => getByText("Monday")).then(() => {
-    fireEvent.click(getByText("Tuesday"));
-    expect(getByText("Leopold Silvers")).toBeInTheDocument();
-  });
+		await waitForElement(() => getByText("Monday"));
+
+		fireEvent.click(getByText("Tuesday"));
+
+		expect(getByText("Leopold Silvers")).toBeInTheDocument();
+	});
+
+	it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
+		const { container } = render(<Application />);
+		const studentName = "Lydia Miller-Jones";
+
+		await waitForElement(() => getByText(container, "Archie Cohen"));
+
+		const appointment = getAllByTestId(container, "appointment")[0];
+
+		fireEvent.click(getByAltText(appointment, "Add"));
+
+		fireEvent.change(getByTestId(appointment, "student-name-input"), {
+			target: { value: studentName },
+		});
+
+		fireEvent.click(getByAltText(appointment, "Tori Malcolm"));
+
+		fireEvent.click(getByText(appointment, "Save"));
+
+		console.log(prettyDOM(appointment));
+	});
 });
