b07a18c436b198a1fcd4c042cc67998005f43b71
Carl Comtois <carlmartonfi@hotmail.com>
2020-10-07 17:08:37 -0600

    Try to fix url

--- src/hooks/useApplicationData.jsx
+++ src/hooks/useApplicationData.jsx
@@ -68,7 +68,7 @@ const useApplicationData = () => {
 
 	// Websocket connection that listens for interview changes
 	React.useEffect(() => {
-		const webSocket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL || "");
+		const webSocket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL || "/");
 
 		webSocket.onmessage = event => {
 			const data = JSON.parse(event.data);
