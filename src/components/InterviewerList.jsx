a1533f151bed7883494c6481484d6b55045e643c
Carl Comtois <carlmartonfi@hotmail.com>
2020-10-07 16:17:45 -0600

    Format with prettier

--- src/components/InterviewerList.jsx
+++ src/components/InterviewerList.jsx
@@ -3,24 +3,24 @@ import InterviewerListItem from "components/InterviewerListItem";
 import "components/InterviewerList.scss";
 
 const InterviewerList = props => {
-  const interviewers = props.interviewers.map(interviewer => {
-    return (
-      <InterviewerListItem
-        key={interviewer.id}
-        name={interviewer.name}
-        avatar={interviewer.avatar}
-        selected={interviewer.id === props.value}
-        setInterviewer={() => props.onChange(interviewer.id)}
-      />
-    );
-  });
+	const interviewers = props.interviewers.map(interviewer => {
+		return (
+			<InterviewerListItem
+				key={interviewer.id}
+				name={interviewer.name}
+				avatar={interviewer.avatar}
+				selected={interviewer.id === props.value}
+				setInterviewer={() => props.onChange(interviewer.id)}
+			/>
+		);
+	});
 
-  return (
-    <section className="interviewers">
-      <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list">{interviewers}</ul>
-    </section>
-  );
+	return (
+		<section className="interviewers">
+			<h4 className="interviewers__header text--light">Interviewer</h4>
+			<ul className="interviewers__list">{interviewers}</ul>
+		</section>
+	);
 };
 
 export default InterviewerList;
--- src/components/InterviewerListItem.jsx
+++ src/components/InterviewerListItem.jsx
@@ -1,31 +1,22 @@
-import React from 'react';
+import React from "react";
 import classnames from "classnames";
 import "components/InterviewerListItem.scss";
 
+const InterviewerListItem = props => {
+	const interviewerListItemClass = classnames("interviewers__item", {
+		"interviewers__item--selected": props.selected,
+	});
 
-const InterviewerListItem = (props) => {
+	const imgClass = classnames("interviewers__item-image", {
+		"interviewers__item--selected-image": props.selected,
+	});
 
-  const interviewerListItemClass = classnames("interviewers__item", {
-    "interviewers__item--selected": props.selected
-  });
-
-  const imgClass = classnames("interviewers__item-image", {
-    "interviewers__item--selected-image": props.selected
-  });
-
-  return (
-    <li
-      className={interviewerListItemClass}
-      onClick={props.setInterviewer}
-    >
-      <img
-        className={imgClass}
-        src={props.avatar}
-        alt={props.name}
-      />
-      {props.selected && props.name}
-    </li>
-  );
+	return (
+		<li className={interviewerListItemClass} onClick={props.setInterviewer}>
+			<img className={imgClass} src={props.avatar} alt={props.name} />
+			{props.selected && props.name}
+		</li>
+	);
 };
 
 export default InterviewerListItem;
